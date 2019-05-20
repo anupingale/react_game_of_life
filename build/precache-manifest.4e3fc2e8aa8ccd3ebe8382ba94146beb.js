ecf19c8f9092591c84dcdb1949b59b97e328629c
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2019-05-20 18:42:49 +0530

    Added the gh page build.

---
+++ build/precache-manifest.4e3fc2e8aa8ccd3ebe8382ba94146beb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d248fb5dbb956eb07fd",
+    "url": "/anupingale/react_game_of_life/static/js/runtime~main.6b9fa964.js"
+  },
+  {
+    "revision": "01f242cd2e1f325a312e",
+    "url": "/anupingale/react_game_of_life/static/js/main.771cb827.chunk.js"
+  },
+  {
+    "revision": "7429e8b1db066b643a22",
+    "url": "/anupingale/react_game_of_life/static/js/2.34806e30.chunk.js"
+  },
+  {
+    "revision": "01f242cd2e1f325a312e",
+    "url": "/anupingale/react_game_of_life/static/css/main.aae13886.chunk.css"
+  },
+  {
+    "revision": "620c1f84764b799e02b230d1724d2ed4",
+    "url": "/anupingale/react_game_of_life/index.html"
+  }
+];
